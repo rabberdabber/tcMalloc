cda9d3b1f67b3a2e4d1f691bd7fd1f0beff6ef55
rabberdabber <60803336+rabberdabber@users.noreply.github.com>
2021-05-12 19:56:58 +0900

    Update tc_malloc.c

--- tc_malloc.c
+++ tc_malloc.c
@@ -682,4 +682,60 @@ tc_free(void *ptr)
     
 }
 
+char testmem[1<<23];
+void*perthread(void*x);
+
+int main(){
+    memset(testmem,'a',1<<23);
+    void* pageheap = tc_central_init();
+    if(pageheap == NULL){
+        printf("central initialization failed\n");
+        return 0;
+    }
+    else printf("central init success\n");
+
+    pthread_t thread[1000];
+    for (int i=0;i<1000;i++){
+        if(pthread_create(&thread[i], NULL, perthread, NULL)){
+            printf("failed pthread create\n");
+            return 0;
+        }
+    }
+    for(int j=0;j<1000;j++){
+        if(pthread_join(thread[j],NULL)){
+            printf("pthread join failed\n");
+            return 0;
+        }
+    }
+    printf("success\n");
+
+}
+
+void *perthread(void* x){
+    void* threadcache = tc_thread_init();
+    if(threadcache == NULL){
+        printf("thread init failed\n");
+        return NULL;
+    }
+    char*ptr1[100];
+    char*ptr2[100];
+    char*ptr3[10];
+    for(int i=0;i<100;i++){
+        ptr1[i] = (char*)tc_malloc(31);
+        memset(ptr1[i],'a',31);
+        ptr2[i] = (char*)tc_malloc(100);
+        memset(ptr2[i],'b',100);
+    }
+    for(int i=0;i<10;i++){
+        ptr3[i] = (char*)tc_malloc(64*1024);
+        memset(ptr3[i],'c',64*1024);
+    }
+    // printing out first
+    for(int i=0;i<100;i++){
+        printf("ptr1: %s\n",ptr1[i]);
+    }
+
+    printf("perthread success\n");
+    return NULL;
+}
 
